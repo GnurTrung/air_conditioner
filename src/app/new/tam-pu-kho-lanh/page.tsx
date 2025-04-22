353674f88e90c28b397d1bbce639853f50059343
GnurTrunG <trungvv@ftech.ai>
2025-04-22 21:38:39 +0700

    update

---
+++ src/app/new/tam-pu-kho-lanh/page.tsx
@@ -0,0 +1,206 @@
+"use client";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Page() {
+  return (
+    <div>
+      <Header />
+      <div className="layout py-20 sm:max-w-[50%]">
+        <h1 className="text-3xl font-bold text-font-title">
+          Quy trình thi công tấm panel PU kho lạnh tiêu chuẩn
+        </h1>
+        <p className="text-gray-600 text-end mb-4">Cập nhật ngày: 15/04/2024</p>
+        <span className="text-gray-600 italic">
+          <span className="text-black font-bold">Tấm panel PU kho lạnh</span> là
+          gì? Lợi ích đối với kho lạnh như thế nào? Quá trình thi công ra sao?
+          Đây là những câu hỏi phổ biến mà nhiều người có ý định làm kho đông
+          lạnh quan tâm. Nhằm đảm bảo hiểu rõ hơn về điều mình đang làm, cũng
+          như là làm như thế nào để tối ưu chất lượng. Ở bài viết này hãy cùng
+          Cơ Điện Lạnh Bắc Ninh giải đáp cụ thể. Mong rằng những thông tin chúng
+          tôi mang đến sẽ hữu ích cho bạn.
+        </span>
+        <img
+          src="/assets/images/news/pu.jpg"
+          alt="Tấm Panel PU"
+          className="w-full rounded-lg my-6 max-h-[500px] object-cover"
+        />
+
+        <div className=" mt-10">
+          {/* Tiêu đề chính */}
+          <h1 className="text-2xl font-bold text-blue-800 mb-4 text-font-title">
+            Ứng dụng của tấm panel PU kho lạnh hiện nay
+          </h1>
+
+          {/* Đoạn giới thiệu */}
+          <p className="text-gray-700 mb-6">
+            Tấm panel PU kho lạnh không còn xa lạ gì, bởi vì đây là vật liệu
+            khắc phục được những hư hại trong các kho đông lạnh. Vải trở chính
+            của nó là cách âm, cách nhiệt với hiệu quả tốt hơn. Để kiểm đồ là
+            rất nhiều những ưu điểm tuyệt vời giúp cho chất lượng của kho lạnh
+            trở nên ưu việt hơn. Bao gồm như sau:
+          </p>
+
+          {/* Danh sách ưu điểm */}
+          <ul className="list-disc list-inside space-y-3 text-gray-700">
+            <li>
+              Tạo độ bền vững tối ưu cho các công trình kho lạnh. Khả năng chống
+              chịu lực rất tốt. Nhờ thế nên nó khắc họa ra tính trạng và đáp ứng
+              được các yêu cầu để không bị gây ra những yếu tố không tốt như là…
+            </li>
+            <li>
+              Làm từ những chất liệu cao cấp, thân thiện. Vì thế nên hết sức an
+              toàn cho môi trường cũng như sức khỏe của người sử dụng.
+            </li>
+            <li>
+              Chống thấm tối ưu. Đây là ưu điểm cực kỳ tuyệt vời của kho lạnh
+              chứa các loại xây dựng ngoài trời. Nó sẽ hạn chế khả năng nước
+              thấm vào bên trong kho lạnh, gây ẩm hướng hư hại trong kho.
+            </li>
+            <li>
+              Trọng lượng nhẹ, dễ dàng mang vác và vận chuyển. Nhờ thế nên quá
+              trình thi công cũng trở nên tiện lợi hơn.
+            </li>
+            <li>
+              Có thể tái sử dụng nhiều lần với nhiều mục đích khác nhau. Vì thế
+              nên cực kỳ tiết kiệm. Đặc biệt chi phí cũng rẻ, là một giải pháp
+              cực kỳ kinh tế nguồn vốn của bạn có hạn.
+            </li>
+            <li>
+              Dễ dàng cho quá trình bảo trì, bảo dưỡng hoặc tháo lắp thay thế.
+              Tham khảo thêm tại{" "}
+              <a href="#" className="text-blue-500 hover:underline">
+                đây
+              </a>
+              .
+            </li>
+          </ul>
+
+          {/* Đoạn kết luận */}
+          <p className="text-gray-700 mt-6">
+            Chúng ta có thể nhận thấy rằng: Những ưu điểm tuyệt vời này khiến
+            quyết định sử dụng tấm panel PU kho lạnh trở nên đúng đắn hơn bao
+            giờ hết. Nó giúp mang đến chất lượng lâu bền, từ đó thao tác có ý
+            nghĩa hoạt động tiết kiệm khác có ích cho kho lạnh.
+          </p>
+        </div>
+
+        <div className="mx-auto py-10 space-y-6 text-gray-800">
+          <h1 className="text-3xl font-bold text-blue-800 text-font-title">
+            Khái niệm và đặc điểm của tấm panel PU kho lạnh
+          </h1>
+
+          <p>
+            Lợi ích của tấm panel PU thật nổi trội, đúng không nào? Vậy khái
+            niệm và đặc điểm của vật liệu này như thế nào? Các bạn liệu đã hiểu
+            rõ? Hãy tiếp tục theo dõi những phần tiếp theo để tìm kiếm sự giải
+            đáp!
+          </p>
+
+          <section>
+            <h2 className="text-2xl font-semibold text-blue-800 mt-6 mb-2 text-font-title">
+              Khái niệm
+            </h2>
+            <ul className="list-disc pl-6 space-y-2">
+              <li>
+                Tấm panel PU của kho lạnh nói một cách dễ hiểu là tấm cách nhiệt
+                được sử dụng trong kho lạnh. Được làm từ nhựa PU (loại nhựa phổ
+                biến với chất lượng cao hiện nay). Những tấm nhựa này khi lắp
+                trong kho lạnh sẽ được liên kết lại với nhau bằng khoá camlock
+                hay gờ âm dương. Đảm bảo độ chắc chắn lâu dài, lên đến hàng chục
+                năm.
+              </li>
+              <li>
+                Nhằm mục đích chính là giữ cho khí lạnh bên trong không gian kho
+                lạnh không bị lọt ra ngoài. Cũng như là khí nóng bên ngoài không
+                bị tràn vào trong. Có thể nói là “nội bất xuất, ngoại bất nhập”.
+                Điều này sẽ giúp đông lạnh thực phẩm, nông sản cũng như các sản
+                phẩm khác với hiệu quả tối ưu. Không bị hụt hơi do nhiệt độ thay
+                đổi đột ngột.
+              </li>
+            </ul>
+          </section>
+
+          <section>
+            <h2 className="text-2xl font-semibold text-blue-800 mt-6 mb-2 text-font-title">
+              Đặc điểm
+            </h2>
+            <ul className="list-disc pl-6 space-y-2">
+              <li>
+                Về đặc điểm thì thường những tấm nhựa này có độ dày khoảng 5cm
+                cho đến 20cm. Hai mặt của tấm nhựa xốp PU sẽ được bao bọc bởi
+                các chất liệu như inox hoặc tole để đảm bảo độ bền và tính thẩm
+                mỹ.
+              </li>
+              <li>
+                Chiều rộng, chiều dài thì lần lượt nằm trong những khoảng như
+                sau: 1m cho đến 1,17m và 1,2m cho đến 6m.
+              </li>
+              <li>
+                Thêm vào đó, tấm panel PU khi thi công kho lạnh cũng có khối
+                lượng rất nhẹ, chỉ khoảng từ 40 – 45kg/m3.
+              </li>
+            </ul>
+          </section>
+        </div>
+
+        <img
+          src="/assets/images/news/pu_2.jpg"
+          alt="Tấm Panel PU"
+          className="w-full rounded-lg my-6 max-h-[500px] object-cover"
+        />
+
+        <section className="mx-auto px-4 py-8 text-gray-800 space-y-6">
+          <h2 className="text-2xl font-semibold text-blue-800 text-font-title">
+            Thi công tấm panel PU như thế nào đạt chuẩn?
+          </h2>
+
+          <p>
+            Thi công tấm panel PU bao gồm các bước tiêu chuẩn như sau: Khảo sát
+            mặt bằng (số liệu, độ phẳng,...) → Lắp vỏ kho lạnh (tức là phần
+            tường bao ở phía bên ngoài) → Lắp đặt hệ thống panel PU → Lắp hệ
+            thống cửa của kho lạnh (thường là chỉ cửa chính để di chuyển) → Hoàn
+            thiện dự án, nghiệm thu.
+          </p>
+
+          <p>
+            Những bước này cần được thực hiện một cách tỉ mỉ, bài bản, cẩn thận
+            và chắc chắn. Đội ngũ thi công phải có kinh nghiệm với độ chuyên
+            nghiệp cao. Đi kèm đó là vật liệu chính hãng cao cấp, các vật chất
+            phục vụ thi công cũng cần đảm bảo hiện đại, tiên tiến.
+          </p>
+
+          <p className="italic text-gray-700">
+            Và những yếu tố này thì Công ty TNHH Ngôi Sao Xanh Kinh Bắc đều có.
+            Chúng tôi cam kết mang đến sản phẩm, dịch vụ tối ưu chất lượng với
+            chi phí rẻ. Hãy liên hệ cho chúng tôi ngay khi có nhu cầu lắp{" "}
+            <strong>tấm panel PU kho lạnh</strong> nhé! Cám ơn vì đã đón đọc bài
+            viết này.
+          </p>
+
+          <div className="pt-6">
+            <h3 className="text-lg font-semibold text-gray-900">
+              Thông tin liên hệ:
+            </h3>
+            <div className="mt-2 space-y-1">
+              <p>
+                <strong>CÔNG TY TNHH Ngôi Sao Xanh Kinh Bắc</strong>
+              </p>
+              <p>
+                <strong>Địa chỉ:</strong> LK36-09, Khu đô thị Sing Garden, Phù
+                Chẩn, Từ Sơn, Bắc Ninh
+              </p>
+              <p>
+                <strong>SDT:</strong> 0868 632 999
+              </p>
+              <p>
+                <strong>Email:</strong> ngoisaoxanhkinhbac@gmail.com
+              </p>
+            </div>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+}
